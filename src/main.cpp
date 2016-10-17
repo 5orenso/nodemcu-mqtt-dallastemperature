39d5c9e1fc4d616d68cd3bf37ecffc3d43101b23
Øistein Sørensen <sorenso@gmail.com>
2016-10-17 17:13:10 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+/*
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define SLEEP_DELAY_IN_SECONDS  30
+
+#define ONE_WIRE_BUS 2  // DS18B20 on arduino pin2 corresponds to D4 on physical board
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+
+const char* ssid = "WIFI_SSID";
+const char* password = "WIFI_PASSWORD";
+
+const char* mqtt_server = "MQTT_SERVER";
+const int   mqtt_port = MQTT_PORT;
+
+const char* outTopic = "MQTT_OUT_TOPIC"
+const char* inTopic = "MQTT_IN_TOPIC"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int chipId = ESP.getChipId();
+
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+        // it is acive low on the ESP-01)
+    } else { }
+}
+
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        // Attempt to connect
+        if (client.connect(clientId.c_str())) {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            // client.publish(outTopic, "{ \"chipId\": chipId, \"ping\": \"hello world\" }");
+            // ... and resubscribe
+            client.subscribe(inTopic);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(callback);
+}
+
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+    long now = millis();
+
+    if (now - lastMsg > 5000) {
+        lastMsg = now;
+        float temp;
+        char temperatureStr[10];
+        DS18B20.begin();
+        DS18B20.requestTemperatures();
+        temp = DS18B20.getTempCByIndex(0);
+        dtostrf(temp, 3, 1, temperatureStr); //handled in sendTemp()
+        snprintf (msg, 75, "{ \"chipId\": %d, \"temperature\": %s }", chipId, temperatureStr);
+        Serial.print("Publish message: ");
+        Serial.println(msg);
+        client.publish(outTopic, msg);
+        Serial.print("Entering deep sleep mode for ");
+        Serial.print(SLEEP_DELAY_IN_SECONDS);
+        Serial.println(" seconds...");
+        ESP.deepSleep(SLEEP_DELAY_IN_SECONDS * 1000000, WAKE_RF_DEFAULT);
+    }
+}
